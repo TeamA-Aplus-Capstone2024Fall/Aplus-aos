a176689869bad391256bfea4b4a0633bdf6f5afd
최진욱 <jinwook.fe@gmail.com>
2024-10-04 13:32:53 +0900

    [feat] 뒤로가기 추가

--- app/src/main/java/com/example/aplus_aos/MainActivity.kt
+++ app/src/main/java/com/example/aplus_aos/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -15,8 +16,22 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.example.aplus_aos.ui.theme.Aplus_AOSTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (::webView.isInitialized && webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+
         setContent {
             Aplus_AOSTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -25,30 +40,57 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun WebViewScreen(url: String) {
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = object: WebViewClient() {
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        super.onPageFinished(view, url)
+    @Composable
+    fun WebViewScreen(url: String) {
+        AndroidView(
+            factory = { context ->
+                webView = WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                        }
                     }
+                    loadUrl(url)
                 }
-                loadUrl(url)
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    )
-}
+                webView
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun WebViewPreview() {
-    Aplus_AOSTheme {
-        WebViewScreen("https://skku-aplus-web.vercel.app/")
+    @Preview(showBackground = true)
+    @Composable
+    fun WebViewPreview() {
+        Aplus_AOSTheme {
+            WebViewScreen("https://skku-aplus-web.vercel.app/")
+        }
     }
 }
+
+//@Composable
+//fun WebViewScreen(url: String) {
+//    AndroidView(
+//        factory = { context ->
+//            WebView(context).apply {
+//                settings.javaScriptEnabled = true
+//                webViewClient = object: WebViewClient() {
+//                    override fun onPageFinished(view: WebView?, url: String?) {
+//                        super.onPageFinished(view, url)
+//                    }
+//                }
+//                loadUrl(url)
+//            }
+//        },
+//        modifier = Modifier.fillMaxSize()
+//    )
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun WebViewPreview() {
+//    Aplus_AOSTheme {
+//        WebViewScreen("https://skku-aplus-web.vercel.app/")
+//    }
+//}
